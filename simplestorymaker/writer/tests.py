7829453c4edf8e8676b71256e810254c5cd5c1ff
Mike Jones <mike@westerncapelabs.com>
2013-06-17 09:13:32 +0200

    Added role and story save tests

--- simplestorymaker/writer/tests.py
+++ simplestorymaker/writer/tests.py
@@ -1,16 +1,54 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+These will execute when you run "manage.py test".
 
-Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.utils import timezone
+
+from writer.models import Role, Story
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class WriterTests(TestCase):
+    def test_role_creation(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests that roles are createable
         """
-        self.assertEqual(1 + 1, 2)
+        new_role = Role(name="Test Role")
+        self.assertEqual(new_role.name, "Test Role")
+
+    def test_role_save(self):
+        """
+        Tests that roles are saveable
+        """
+        new_role = Role(name="Test Role")
+        new_role.save()
+        self.assertEqual(new_role.id, 1)
+
+    def test_story_creation(self):
+        """
+        Tests that stories are createable
+        """
+        new_role = Role(name="Test Role")
+        new_role.save()
+        # role = Role.objects.get(n)
+        now = timezone.now()
+        new_story = Story(reason='test reason', role=new_role,
+                          goal='test goal', create_date=now)
+        self.assertEqual(new_story.reason, "test reason")
+        self.assertEqual(new_story.goal, "test goal")
+        self.assertEqual(new_story.create_date, now)
+        self.assertEqual(new_story.role, new_role)
+
+    def test_story_save(self):
+        """
+        Tests that stories are saveable
+        """
+        new_role = Role(name="Test Role")
+        new_role.save()
+        # role = Role.objects.get(n)
+        now = timezone.now()
+        new_story = Story(reason='test reason', role=new_role,
+                          goal='test goal', create_date=now)
+        new_story.save()
+        self.assertEqual(new_story.id, 1)
